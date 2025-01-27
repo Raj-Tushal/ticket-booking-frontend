5a87cdc6b82068e98995add6e3e07496e122000f
Raj Tushal <tushalkhatri902@gmail.com>
2025-01-27 14:15:20 +0500

    production issue resolved

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL ||  process.env.REACT_APP_Production_URL
+  // baseURL: process.env.REACT_APP_BASE_URL ||  process.env.REACT_APP_Production_URL
+  baseURL: "https://ticket-booking-backend-production-3958.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
