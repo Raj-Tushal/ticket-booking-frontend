d15a129d100e49b623db8d0e7f4774521c2667dd
Raj Tushal <tushalkhatri902@gmail.com>
2025-01-27 21:01:59 +0500

    home page responsive ness done

--- src/components/EmailList.jsx
+++ src/components/EmailList.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function EmailList() {
   return (
     <div className='w-full h-60 bg-[#003173] dark:bg-[#1E1E1E] flex justify-center items-center flex-col gap-4 text-white
-    max-sm:p-5 '>
-         <h1 className='text-5xl font-bold'>Save time, Save money!</h1>
+    max-sm:p-7 '>
+         <h1 className='text-5xl font-bold max-sm:text-4xl'>Save time, Save money!</h1>
          <p className='text-lg'>sign Up and We'll Send the Best Deals to you</p>
         <div className='flex gap-2'>
         <input type="text" className='rounded-lg p-2 '  placeholder='Your Email' />
